5b1385a1ec4b048a25a810b1effc3a86fc2734b8
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-07-27 10:45:46 -0500

    createad a simple script that saves text to the storage

---
+++ popup.js
@@ -0,0 +1,15 @@
+const status = document.getElementById('status')
+const btn = document.getElementById("save-btn")
+
+async function loadStatus() {
+    const data = await browser.storage.local.get('scrap');
+    if (data.scrap) status.textContent = `Saved "$data.scrap"`;
+}
+
+btn.addEventListener('click', async () => {
+    const note = 'Hello from ${new Date().toLocaleString()}';
+    await browser.storage.local.set({ scrap: note });
+    loadStatus();
+})
+
+loadStatus();
