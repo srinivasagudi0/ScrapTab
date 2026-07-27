8a904139fcbffca38cb1dd4063aeb5cf0ae58283
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-07-27 10:55:41 -0500

    bug fixed

--- popup.js
+++ popup.js
@@ -3,11 +3,11 @@ const btn = document.getElementById("save-btn")
 
 async function loadStatus() {
     const data = await browser.storage.local.get('scrap');
-    if (data.scrap) status.textContent = `Saved "$data.scrap"`;
+    if (data.scrap) status.textContent = `Saved: "${data.scrap}"`;
 }
 
 btn.addEventListener('click', async () => {
-    const note = 'Hello from ${new Date().toLocaleString()}';
+    const note = `Hello from ${new Date().toLocaleTimeString()}`;
     await browser.storage.local.set({ scrap: note });
     loadStatus();
 })
