4af43f6ef6601b16fe1636e9db233520d41d5de6
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-07-27 11:27:04 -0500

    created the roight click menu on istall

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
 browser.runtime.onInstalled.addListener(() => {
-  console.log('Scraplet installed');
+  browser.contextMenus.create({ id: "save-selection", title: "Add to Scraplet", contexts: ["selection"] });
+  browser.contextMenus.create({ id: "save-image", title: "Add Image to Scraplet", contexts: ["image"] });
 });
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId==="save-selection") {
+    browser.tabs.sendMessage(tab.id, { action: "capture-selection", text: info.selectionText , url: tab.url });
+  } else if (info.menuItemId==="save-image") {
+    browser.tabs.sendMessage(tab.id, { action: "capture-image", imageUrl: info.srcUrl, url: tab.url });
+  }
+} )
+
